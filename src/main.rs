065003936ab7d126f1cd4a842e0b66ecd50ebd3c
3QNRpDwD <team.project.longinus@gmail.com>
2023-11-17 14:29:05 +0900

    3차 업데이트, 서버 실행위치와 상관없이 요청 처리가 가능하도록 변경됨

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::{fs::File, io::Read};
+use std::env;
+use std::path::Path;
 
 use actix_web::{App, get, HttpRequest, HttpResponse, HttpServer, Responder};
 
 #[get("/")]// 메인폼 요청 처리
 async fn index() -> impl Responder {
     let mut content:String = String::new();
-    match read_file("src/resources/html/index.html".to_string()) {
+    match path("html","index.html") {
         Ok(v) => content = v,
         Err(_) => content = "파일을 찾을수없습니다.".to_string()
     }
@@ -22,31 +24,53 @@ async fn handle_req(req: HttpRequest) -> impl Responder {
 
 // 파일의 경로를 설정하고 read_file 함수를 사용해서 불러옴
 fn path(folder: &str, file: &str) -> Result<String, Vec<u8>> {
-    match read_file(format!( "src/resources/{}/{}", folder,   file)) {
+    println!("요청 경로 : /{}/{}", folder, file);
+    // Use unwrap_or_else to provide a default value in case of an error.
+    let file_path = env::current_exe()
+        .map(|exe_path| exe_path.parent().map_or(Default::default(), |exe_dir| exe_dir.join(Path::new(&format!( "resources/{}/{}", folder,   file)))))
+        .unwrap_or_else(|e| {
+            eprintln!("Failed to get the current executable path: {}", e);
+            Default::default()
+        });
+
+    println!("실제 경로 : {:?}", file_path);
+    match read_file(file_path.as_path()) {
         Ok(v) => { println!(" 텍스트 파일: {}", file); Ok(v)},
         Err(e) => { println!(" 바이너리 파일: {}", file); Err(e) }
     }
 }
 
 // 실제로 파일을 읽는 함수
-fn read_file(path: String) -> Result<String, Vec<u8>> {
+fn read_file(path: &Path) -> Result<String, Vec<u8>> {
     let binary_file_list: Vec<&str> = vec!["ico", "png", "webp", "ttf", "woff2", "woff"];
-    let mut contents: Vec<u8> = Vec::new();
-    let split_extension: Vec<&str> = path.split(".").collect();
+    let mut contents = Vec::new();
 
-    match File::open(path.clone()) {
-        Ok(mut file) => {
+    // Use map_err to convert the error to a string before returning it.
+    File::open(path)
+        .map_err(|e| {
+            println!("파일 {:?} 읽기 오류 : {}", path, e);
+            e.to_string()
+        })
+        .and_then(|mut file| {
+            // Use ? operator for early return in case of an error.
             file.read_to_end(&mut contents).expect("파일 읽기 실패");
-        }
-        Err(_) => return Err(contents),
-    };
+            Ok(())
+        })
+        .ok(); // Ignore the result since it's already logged.
 
-    for &list_extension in binary_file_list.iter() {
-        if split_extension.ends_with(&[list_extension]) {
+    let split_extension: Vec<&str> = path
+        .to_str()
+        .unwrap_or_default()
+        .split('.')
+        .collect();
+
+    if let Some(&list_extension) = split_extension.last() {
+        if binary_file_list.contains(&list_extension) {
             return Err(contents);
         }
     }
 
+    println!("파일 읽기 성공 : {}", path.to_string_lossy());
     String::from_utf8(contents.clone()).map_err(|_| contents)
 }
 
