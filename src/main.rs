611cfbc2582694f3f1dab4f8276c7ad8a17942ad
3QNRpDwD <team.project.longinus@gmail.com>
2023-12-18 19:28:08 +0900

    버그픽스 22

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-
-use std::{fs::File, io::Read, env , path::Path};
-use std::os::raw::c_char;
-use serde_with::serde_as;
-use serde::{Serialize, Deserialize};
-use serde_json::from_str;
-use actix_web::{middleware, App, get, HttpRequest, HttpResponse, HttpServer, Responder, web };
+use actix_web::{
+    get, middleware::Logger, web::post, web::resource, web::route, web::Data, web::Json,
+    web::Redirect, App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
 use log::info;
-use regex::Regex;
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
+use serde_with::serde_as;
+use std::sync::Mutex;
+use std::{env, fs::File, io::Read, path::Path};
 use uuid::Uuid;
-
 
 #[serde_as]
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone)]
 struct Stamp {
     stampId: String,
-    stampLocation:String,
+    stampLocation: String,
     stampName: String,
-    stampDesc: String
+    stampDesc: String,
 }
 
 #[serde_as]
@@ -32,16 +32,28 @@ struct UserName {
 
 #[serde_as]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-struct Userid {
+struct User {
     user_name: String,
-    user_id: Uuid
+    user_id: Uuid,
 }
 
-#[get("/")]// 메인폼 요청 처리
+#[derive(Clone)]
+struct AddressInfo {
+    address: String,
+    port: u16,
+    protocol: String,
+}
+
+#[derive(Clone)]
+struct UserList {
+    users: Vec<User>,
+}
+
+#[get("/")] // 메인폼 요청 처리
 async fn index() -> impl Responder {
-    match path("html","index.html").await {
+    match path("html", "index.html").await {
         Ok(v) => HttpResponse::Ok().body(v),
-        Err(_) => handle_404().await
+        Err(_) => handle_404().await,
     }
 }
 
@@ -54,75 +66,81 @@ async fn handle_req(req: HttpRequest) -> impl Responder {
     let folder = req.match_info().get("folder").unwrap();
     match path(&*folder, req.match_info().query("file")).await {
         Ok(result) => HttpResponse::Ok().body(result),
-        Err(e) =>  HttpResponse::Ok().body(e)
+        Err(e) => HttpResponse::Ok().body(e),
     }
 }
 
 #[get("/check")] // 동적 페이지 요청 처리
 async fn handle_check(req: HttpRequest) -> impl Responder {
     // web::Redirect::to(format!("https://stamptour.space/stamp?{}", req.query_string())).permanent() // 진짜 쓸거
-    web::Redirect::to(format!("http://192.168.0.9:80/stamp?{}", req.query_string())).permanent() // 디버깅용
+    Redirect::to(format!("/stamp?{}", req.query_string())).permanent()
+    // 디버깅용
 }
 
 #[get("/stamp")]
-async fn handle_stamp(req: HttpRequest, StampList: web::Data<StampList>) -> impl Responder {
-    let query_stamp = req.query_string().split("s=").collect::<Vec<_>>()[1];
+async fn handle_stamp(req: HttpRequest, StampList: Data<StampList>) -> impl Responder {
     let StampList = StampList.get_ref();
 
+    if !req.query_string().contains("s=") {
+        return handle_404().await;
+    }
+
     for stamp in &StampList.stampList {
-        if stamp.stampId == query_stamp {
+        if stamp.stampId == req.query_string().split("s=").collect::<Vec<_>>()[1] {
             return HttpResponse::Ok().body(format_file(&stamp.stampName).await);
         }
     }
 
-    // If no matching stamp is found, return a default response
-    HttpResponse::Ok().body(format_file(&query_stamp).await)
+    handle_404().await
 }
 
-async fn handle_login(user_name: web::Json<UserName>, req: HttpRequest) -> HttpResponse {
-    HttpResponse::Ok().json(user_registration(user_name.0))
+async fn handle_login(user_name: Json<UserName>, user_list: Data<UserList>) -> HttpResponse {
+    println!("{:?}", user_name.0);
+    let user = user_registration(user_name.0);
+    // user_list.users.append(user);
+    HttpResponse::Ok().json(user)
 }
 
-fn user_registration(user_name: UserName) -> Userid{
-    Userid {
+async fn user_registration(user_name: UserName) -> User {
+    User {
         user_name: user_name.username,
-        user_id: uuid::Uuid::new_v4(),
+        user_id: Uuid::new_v4(),
     }
 }
 
-
 fn parse_json() -> StampList {
     // 파일 열기
-    let mut file = File::open("resources/api/stampList.json").expect("Failed to open file");
-
-    // 파일 내용을 문자열로 읽기
-    let mut file_content = String::new();
-    file.read_to_string(&mut file_content)
-        .expect("Failed to read file content");
-
-    // JSON 문자열을 역직렬화하여 구조체로 변환
-    let StampList: StampList = from_str(&file_content).expect("Failed to deserialize JSON");
+    let StampList: StampList = match File::open("resources/api/stampList.json") {
+        Ok(mut file) => {
+            let mut file_content = String::new();
+            file.read_to_string(&mut file_content).expect("");
+            from_str(&file_content).expect("")
+        }
+        Err(_) => StampList {
+            stampList: vec![Stamp {
+                stampId: "".to_string(),
+                stampLocation: "".to_string(),
+                stampName: "".to_string(),
+                stampDesc: "".to_string(),
+            }],
+        },
+    };
 
     StampList
-
 }
 
 async fn format_file(stamp_id: &str) -> String {
     match path("html", "stamp_check_page.html").await {
-        Ok(file) => {
-            file.replace("%STAMP_ID%", stamp_id)
-        },
-        Err(_) => "Fail to format".to_string()
+        Ok(file) => file.replace("%STAMP_ID%", stamp_id),
+        Err(_) => "Fail to format".to_string(),
     }
 }
 
 #[get("/{file}")]
 async fn handle_html(req: HttpRequest) -> impl Responder {
-    let split_str: Vec<&str> = req.match_info().query("file")
-        .split('.')
-        .collect();
+    let split_str: Vec<&str> = req.match_info().query("file").split('.').collect();
 
-    let formatted_file: String;  // Declare the formatted_file variable without initializing it.
+    let formatted_file: String; // Declare the formatted_file variable without initializing it.
     let file: &str;
 
     if split_str.len() == 1 {
@@ -139,17 +157,20 @@ async fn handle_html(req: HttpRequest) -> impl Responder {
             } else {
                 HttpResponse::Ok().body(result)
             }
-        },
-        Err(_) => handle_404().await
+        }
+        Err(_) => handle_404().await,
     }
 }
-
 
 // 파일의 경로를 설정하고 read_file 함수를 사용해서 불러옴
 async fn path(folder: &str, file: &str) -> Result<String, Vec<u8>> {
     // Use unwrap_or_else to provide a default value in case of an error.
     let file_path = env::current_exe()
-        .map(|exe_path| exe_path.parent().map_or(Default::default(), |exe_dir| exe_dir.join(Path::new(&format!( "resources\\{}\\{}", folder,   file)))))
+        .map(|exe_path| {
+            exe_path.parent().map_or(Default::default(), |exe_dir| {
+                exe_dir.join(Path::new(&format!("resources\\{}\\{}", folder, file)))
+            })
+        })
         .unwrap_or_else(|e| {
             // eprintln!("Failed to get the current executable path: {}", e);
             Default::default()
@@ -157,7 +178,7 @@ async fn path(folder: &str, file: &str) -> Result<String, Vec<u8>> {
 
     match read_file(file_path.as_path()).await {
         Ok(v) => Ok(v),
-        Err(e) =>  Err(e)
+        Err(e) => Err(e),
     }
 }
 
@@ -179,11 +200,7 @@ async fn read_file(path: &Path) -> Result<String, Vec<u8>> {
         })
         .ok(); // Ignore the result since it's already logged.
 
-    let split_extension: Vec<&str> = path
-        .to_str()
-        .unwrap_or_default()
-        .split('.')
-        .collect();
+    let split_extension: Vec<&str> = path.to_str().unwrap_or_default().split('.').collect();
 
     if let Some(&list_extension) = split_extension.last() {
         if binary_file_list.contains(&list_extension) {
@@ -199,36 +216,56 @@ async fn read_file(path: &Path) -> Result<String, Vec<u8>> {
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let StampList: StampList = parse_json();
+    let user_list: Data<Mutex<UserList>> = Data::new(Mutex::new(UserList {
+        users: vec![User {
+            user_name: "".to_string(),
+            user_id: Default::default(),
+        }],
+    }));
+    let stamp_list: StampList = parse_json();
     let args: Vec<String> = env::args().collect();
 
-    // Set default values
-    let default_address = "127.0.0.1".to_string();
-    let default_port = "80".to_string();
-    let default_protocol = "http".to_string();
-
     // Parse command line arguments
-    let address = args.get(1).unwrap_or(&default_address).to_string();
-    let port = args.get(2).unwrap_or(&default_port).parse::<u16>().unwrap_or(80);
-    let protocol = args.get(3).unwrap_or(&default_protocol).to_string();
+    let address_info = AddressInfo {
+        address: args
+            .get(1)
+            .unwrap_or(&String::from("127.0.0.1"))
+            .to_string(),
+        port: args
+            .get(2)
+            .unwrap_or(&String::from("80"))
+            .parse::<u16>()
+            .unwrap(),
+        protocol: args.get(3).unwrap_or(&String::from("http")).to_string(),
+    };
 
-    info!("Rust {} Actix-web server started at http://{}:{}", protocol, address, port);
+    let move_address = address_info.clone();
+
+    info!(
+        "{}",
+        format!(
+            "Rust {protocol} Actix-web server started at {protocol}://{address}:{port}",
+            protocol = address_info.protocol,
+            address = address_info.address,
+            port = address_info.port
+        )
+    );
 
     HttpServer::new(move || {
         App::new()
-            .wrap(middleware::Logger::default())
-            .app_data(web::Data::new(StampList.clone()))
-            .service(web::resource("/login").route(web::post().to(handle_login)))
+            .wrap(Logger::default())
+            .app_data(Data::new(stamp_list.clone()))
+            .app_data(Data::new(move_address.clone()))
+            .app_data(Data::clone(&user_list))
+            .service(resource("/login").route(post().to(handle_login)))
             .service(index)
             .service(handle_check)
             .service(handle_stamp)
             .service(handle_html)
             .service(handle_req)
-            .default_service(web::route().to(handle_404))
+            .default_service(route().to(handle_404))
     })
-        .bind((address.as_str(), port))?
-        .run()
-        .await
+    .bind((address_info.address.as_str(), address_info.port))?
+    .run()
+    .await
 }
-
-
